023d6702cf8fa8824b00b0923a78f287199eebf5
glody007 <glodymbutwile@gmail.com>
2023-07-23 11:11:49 +0200

    fix: Vertical alignment.

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 h-full flex flex-col justify-center">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
