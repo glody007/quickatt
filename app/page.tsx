42b3cd6e14a4f5284532b4113d990b88f12bf45c
glody007 <glodymbutwile@gmail.com>
2023-07-23 10:30:33 +0200

    refactor: Cleaning form.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { UserAuthForm } from "@/components/form/userAuthForm"
+import { HomeIcon } from "lucide-react"
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -15,13 +16,13 @@ export default function AuthenticationPage() {
     <>
       <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          href="/dashboard"
+          href="/"
           className={cn(
             buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
+            "absolute right-4 top-4 md:right-8 md:top-8 flex gap-1"
           )}
         >
-          Login
+          Home
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-slate-900" />
@@ -32,7 +33,7 @@ export default function AuthenticationPage() {
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;"This attendance and visitor record app has been a 
+                &ldquo;This attendance and visitor record app has been a 
                 lifesaver! Keeping track of both attendance and visitors can 
                 be a real challenge, but this app has made it so much easier. 
                 It's like having a dedicated assistant at my fingertips. 
@@ -42,7 +43,7 @@ export default function AuthenticationPage() {
                 saved me countless hours, making my job a whole lot smoother. 
                 Now, I can focus on providing a welcoming and organized environment 
                 for everyone. If you need a reliable solution for attendance 
-                and visitor management, look no further!".&rdquo;
+                and visitor management, look no further!.&rdquo;
               </p>
               <footer className="text-sm">Sofia Davis</footer>
             </blockquote>
@@ -52,11 +53,8 @@ export default function AuthenticationPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Login with your account
+                Sign in with Google
               </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
             </div>
             <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
--- components/form/userAuthForm.tsx
+++ components/form/userAuthForm.tsx
@@ -24,47 +24,28 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-          </div>
-          <Button onClick={() => signIn()} disabled={isLoading} className="bg-green-600 hover:bg-green-400">
-            {isLoading && (
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Google
-          </Button>
-        </div>
-      </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <Button variant="outline" type="button" disabled={isLoading}>
+      <Button 
+        onClick={() => signIn()}
+        size="lg" 
+        variant="outline" 
+        type="button" 
+        disabled={isLoading}
+    >
         {isLoading ? (
           <Loader2 className="mr-2 h-4 w-4 animate-spin" />
         ) : (
-          <Github className="mr-2 h-4 w-4" />
+            <div className="mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20.6" height="24" viewBox="0 0 186.69 190.5">
+                    <g transform="translate(1184.583 765.171)">
+                        <path clip-path="none" mask="none" d="M-1089.333-687.239v36.888h51.262c-2.251 11.863-9.006 21.908-19.137 28.662l30.913 23.986c18.011-16.625 28.402-41.044 28.402-70.052 0-6.754-.606-13.249-1.732-19.483z" fill="#4285f4"/>
+                        <path clip-path="none" mask="none" d="M-1142.714-651.791l-6.972 5.337-24.679 19.223h0c15.673 31.086 47.796 52.561 85.03 52.561 25.717 0 47.278-8.486 63.038-23.033l-30.913-23.986c-8.486 5.715-19.31 9.179-32.125 9.179-24.765 0-45.806-16.712-53.34-39.226z" fill="#34a853"/>
+                        <path clip-path="none" mask="none" d="M-1174.365-712.61c-6.494 12.815-10.217 27.276-10.217 42.689s3.723 29.874 10.217 42.689c0 .086 31.693-24.592 31.693-24.592-1.905-5.715-3.031-11.776-3.031-18.098s1.126-12.383 3.031-18.098z" fill="#fbbc05"/>
+                        <path d="M-1089.333-727.244c14.028 0 26.497 4.849 36.455 14.201l27.276-27.276c-16.539-15.413-38.013-24.852-63.731-24.852-37.234 0-69.359 21.388-85.032 52.561l31.692 24.592c7.533-22.514 28.575-39.226 53.34-39.226z" fill="#ea4335" clip-path="none" mask="none"/>
+                    </g>
+                </svg>
+            </div>
         )}{" "}
-        Github
+        Sign In
       </Button>
     </div>
   )
