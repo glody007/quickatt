876efb94f2b9dc683b7f18841971647e92393b91
glody007 <glodymbutwile@gmail.com>
2023-07-23 14:59:23 +0200

    feat: User nav uesr data.

--- components/userNav.tsx
+++ components/userNav.tsx
@@ -15,17 +15,21 @@ import {
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
   import { useRouter } from "next/navigation"
-  import { signOut } from "next-auth/react";
+  import { signOut, useSession } from "next-auth/react";
   
   export function UserNav() {
     const router = useRouter()
+
+    const { data: session } = useSession();
+    
+    const img = session?.user?.image || ""
 
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
-              <AvatarImage src="/avatars/02.png" alt="@shadcn" />
+              <AvatarImage src={img} alt="Profile image" />
               <AvatarFallback>SC</AvatarFallback>
             </Avatar>
           </Button>
@@ -33,9 +37,9 @@ import {
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">shadcn</p>
+              <p className="text-sm font-medium leading-none">{session?.user?.name}</p>
               <p className="text-xs leading-none text-muted-foreground">
-                admin@gmail.com
+                {session?.user?.email}
               </p>
             </div>
           </DropdownMenuLabel>
