f64615aac3d33842099e1c70f5a75bc27dea8ef4
glody007 <glodymbutwile@gmail.com>
2023-07-26 16:47:32 +0200

    fix: Initial schedules days.

--- lib/utilsBackend.ts
+++ lib/utilsBackend.ts
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
 
 export async function initSchedule(organisationId: string) {
-    const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    const days = ["mon", "tue", "wed", "thu", "fri"]
     const schedules = [
         { activity: "work", startTime: "08:00", endTime: "12:00", timeZone: "Africa/Lubumbashi" },
         { activity: "break", startTime: "12:00", endTime: "13:00", timeZone: "Africa/Lubumbashi" },
